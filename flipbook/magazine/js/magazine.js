a170d38701e1540970429d56202de6c36ca5abdc
annafiu <annafiu78@gmail.com>
2021-08-26 06:06:02 +0700

    add portfolio flipbook

--- flipbook/magazine/js/magazine.js
+++ flipbook/magazine/js/magazine.js
@@ -48,7 +48,7 @@ function loadPage(page, pageElement) {
 
 	// Load the page
 
-	img.attr('src', 'pages/' +  page + '.jpg');
+	img.attr('src', 'https://raw.githubusercontent.com/annafiu/img/main/' +  page + '.jpg');
 
 	loadRegions(page, pageElement);
 
